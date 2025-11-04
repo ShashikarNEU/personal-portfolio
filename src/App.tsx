f4a7174cf62f06b79dcff78ce20b534d7f48772d
Shashikar Anthoni Raj <anthoniraj.s@northeastern.edu>
2025-11-03 21:09:12 -0500

    removed unused useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import About from './components/About/about';
 import Experience from './components/Experience/experience';
 import Education from './components/Education/education'; 
